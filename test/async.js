85b12c34cc7a34a8edbe26d356174cdb039c3bf0
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-16 19:21:18 -0200

    test that we can reconnect and have the same api object!

--- test/async.js
+++ test/async.js
@@ -28,7 +28,6 @@ tape('async', function (t) {
     t.end()
   })
 
-
 })
 
 tape('source', function (t) {
@@ -53,3 +52,80 @@ tape('source', function (t) {
   }))
 
 })
+
+tape('async - error', function (t) {
+  var A = mux(client, null) ()
+
+  var s = A.createStream()
+
+  A.hello('world', function (err, value) {
+    t.ok(err)
+    t.end()
+  })
+
+  s.sink(function (abort, cb) { cb(true) })
+})
+
+
+tape('async - error', function (t) {
+  var A = mux(client, null) ()
+
+  var s = A.createStream()
+
+  A.hello('world', function (err, value) {
+    console.log('CB!')
+    t.ok(err)
+    t.end()
+  })
+
+  s.source(true, function () {})
+})
+
+tape('buffer calls before stream is created', function (t) {
+  var A = mux(client, null) ()
+  var B = mux(null, client) ({
+    hello: function (a, cb) {
+      cb(null, 'hello, '+a)
+    },
+  })
+
+  A.hello('world', function (err, value) {
+    if(err) throw err
+    console.log(value)
+    t.equal(value, 'hello, world')
+    t.end()
+  })
+
+  var s = A.createStream()
+  pull(s, B.createStream(), s)
+
+})
+
+tape('async - error', function (t) {
+  var A = mux(client, null) ()
+
+  var s = A.createStream()
+
+  A.hello('world', function (err, value) {
+    t.ok(err)
+
+    var B = mux(null, client) ({
+      hello: function (a, cb) {
+        cb(null, 'hello, '+a)
+      },
+    })
+
+    var s = A.createStream()
+
+    pull(s, B.createStream(), s)
+
+    A.hello('world', function (err, value) {
+      t.notOk(err)
+      t.equal(value, 'hello, world')
+      t.end()
+    })
+  })
+
+  s.sink(function (abort, cb) { cb(true) })
+})
+
