4d366574da68ec09125560a270cb20866da3b611
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-16 19:10:15 -0200

    abort everything on disconnection

--- pull-weird.js
+++ pull-weird.js
@@ -2,40 +2,47 @@ var pull = require('pull-stream')
 // wrap pull streams around packet-stream's weird streams.
 
 function source (weird) {
-  var buffer = [], ended = false, waiting
-  weird.read = function (data, end) {
-    if(waiting) {
-      var cb = waiting
-      waiting = null
-      cb(ended = ended || end, data)
-    }
-    else
-      (ended = ended || end) || buffer.push(data)
-  }
-  return function (abort, cb) {
-      if(buffer.length) cb(null, buffer.shift())
-      else if(ended) cb(ended)
-      else waiting = cb
-    }
 }
 
 function sink (weird) {
-  return function (read) {
-      pull.drain(function (data) {
-        weird.write(data)
-      }, function (end) {
-        weird.end(end || true)
-      }) (read)
-    }
+  return 
 }
 
 module.exports = function (weird) {
+  var buffer = [], ended = false, waiting
+  weird.read = function (data, end) {
+    console.log('ENDED?', end)
+    ended = ended || end
+    if(waiting) {
+      var cb = waiting
+      waiting = null
+      cb(ended, data)
+    }
+    else
+      if(!ended) buffer.push(data)
+  }
 
   return {
-    source: source(weird),
-    sink: sink(weird)
+    source: function (abort, cb) {
+      if(abort) return weird.write(null, abort), cb(abort)
+      if(buffer.length) cb(null, buffer.shift())
+      else if(ended) cb(ended)
+      else waiting = cb
+    },
+    sink  : function (read) {
+      pull.drain(function (data) {
+        weird.write(data)
+      }, function (end) {
+        weird.write(null, end || true)
+      }) (read)
+    }
   }
 }
 
-module.exports.source = source
-module.exports.sink = sink
+module.exports.source = function (s) {
+  return module.exports(s).source
+}
+module.exports.sink = function (s) {
+  return module.exports(s).sink
+}
+
