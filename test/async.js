62e00d3ca81b2facd7f93a24b2ae021cbb98d3ac
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-16 18:13:36 -0200

    use tape

--- test/async.js
+++ test/async.js
@@ -9,27 +9,47 @@ var client = {
   source: ['stuff']
 }
 
-var A = mux(client, null) ()
-var B = mux(null, client) ({
-  hello: function (a, cb) {
-    cb(null, 'hello, '+a)
-  },
-  stuff: function (b) {
-    return pull.values([1, 2, 3, 4, 5].map(function (a) {
-      return a * b
-    }))
-  }
+tape('async', function (t) {
+
+  var A = mux(client, null) ()
+  var B = mux(null, client) ({
+    hello: function (a, cb) {
+      cb(null, 'hello, '+a)
+    },
+  })
+
+  var s = A.createStream()
+  pull(s, pull.through(console.log), B.createStream(), s)
+
+  A.hello('world', function (err, value) {
+    if(err) throw err
+    console.log(value)
+    t.equal(value, 'hello, world')
+    t.end()
+  })
+
+
 })
 
-var s = A.createStream()
-pull(s, pull.through(console.log), B.createStream(), s)
+tape('source', function (t) {
 
-A.hello('world', function (err, value) {
-  if(err) throw err
-  console.log(value)
+  var A = mux(client, null) ()
+  var B = mux(null, client) ({
+    stuff: function (b) {
+      return pull.values([1, 2, 3, 4, 5].map(function (a) {
+        return a * b
+      }))
+    }
+  })
+
+  var s = A.createStream()
+  pull(s, pull.through(console.log), B.createStream(), s)
+
+  pull(A.stuff(5), pull.collect(function (err, ary) {
+    if(err) throw err
+    console.log(ary)
+    t.deepEqual(ary, [5, 10, 15, 20, 25])
+    t.end()
+  }))
+
 })
-
-pull(A.stuff(5), pull.collect(function (err, ary) {
-  if(err) throw err
-  console.log(ary)
-}))
