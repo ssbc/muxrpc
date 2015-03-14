dfd0e0857016c8145f1c5ec3fb5858491e3289f9
Dominic Tarr <dominic.tarr@gmail.com>
2015-03-14 16:50:23 +1300

    begin rewriting permissions system

--- permissions.js
+++ permissions.js
@@ -14,6 +14,38 @@ function toArray(str) {
   return isArray(str) ? str : str.split('.')
 }
 
+/*
+
+perms:
+
+a given capability may be permitted to call a particular api.
+but only if a perms function returns true for the arguments
+it passes.
+
+suppose, an app may be given access, but may only create functions
+with it's own properties.
+
+create perms:
+  {
+    allow: ['add', 'query'], deny: [...],
+    rules: {
+      add: {
+        call: function (value) {
+          return (value.type === 'task' || value.type === '_task')
+        },
+      query: {
+        call: function (value) {
+          safe.contains(value, {path: ['content', 'type'], eq: 'task'}) ||
+          safe.contains(value, {path: ['content', 'type'], eq: '_task'})
+        },
+        filter: function (value) {
+          return (value.type === 'task' || value.type === '_task')
+        }
+      }
+    }
+  }
+*/
+
 module.exports = function () {
   var whitelist = null
   var blacklist = {}
@@ -36,13 +68,21 @@ module.exports = function () {
     return this
   }
 
-  perms.test = function (name, args) {
+  perms.pre = function (name, args) {
     name = isArray(name) ? name : [name]
     if(whitelist && !u.prefix(whitelist, name))
       return new Error('method:'+name + ' is not on whitelist')
 
     if(blacklist && u.prefix(blacklist, name))
       return new Error('method:'+name + ' is on blacklist')
+  }
+
+  perms.post = function (err, value) {
+    //TODO
+  }
+
+  perms.test = function (name, args) {
+    return perms.pre(name, args)
   }
 
   return perms
