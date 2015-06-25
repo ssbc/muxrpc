203fbf49be2c59495455f9f5b84261a4a8668ce2
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-25 16:47:22 +0200

    test that abort works correctly

--- test/abort.js
+++ test/abort.js
@@ -2,6 +2,7 @@
 var mux  = require('../')
 var tape = require('tape')
 var pull = require('pull-stream')
+var Abortable = require('pull-abortable')
 
 function id (e) { return e }
 
@@ -19,6 +20,7 @@ function abortStream(onAbort, onAborted) {
 
 module.exports = function (serializer) {
   tape('stream abort', function (t) {
+    t.plan(2)
 
     var client = {
       drainAbort: 'sink'
@@ -33,7 +35,6 @@ module.exports = function (serializer) {
           pull.collect(function (err, ary) {
             console.log(ary)
             t.deepEqual(ary, [1, 2, 3])
-            t.end()
           })
         )
       }
@@ -47,20 +48,70 @@ module.exports = function (serializer) {
     var sent = []
 
     pull(
-      pull.values([1,2,3,4,5,6,7,8,9,10]),
+      pull.values([1,2,3,4,5,6,7,8,9,10], function (abort) {
+        t.ok(sent.length < 10, 'sent is correct')
+        t.end()
+      }),
       pull.asyncMap(function (data, cb) {
         setImmediate(function () {
           cb(null, data)
         })
       }),
-      pull.through(sent.push.bind(sent), function (abort) {
-        t.ok(sent.length < 10, 'sent is correct')
-        console.log('abort', abort)
-      }),
+      pull.through(sent.push.bind(sent)),
       A.drainAbort(3)
     )
 
   })
 }
+
+module.exports = function (serializer) {
+  tape('stream abort', function (t) {
+    t.plan(2)
+    var abortable = Abortable()
+    var client = {
+      drainAbort: 'sink'
+    }
+
+    var A = mux(client, null, serializer) ()
+    var B = mux(null, client, serializer) ({
+      drainAbort: function (n) {
+        return pull(
+          pull.through(function () {
+            if(--n) return
+            abortable.abort()
+          }),
+          pull.collect(function (err, ary) {
+            console.log(ary)
+            t.deepEqual(ary, [1, 2, 3])
+          })
+        )
+      }
+    })
+
+    var as = A.createStream()
+    var bs = B.createStream()
+
+    pull(as, abortable, bs, as)
+
+    var sent = []
+
+    pull(
+      pull.values([1,2,3,4,5,6,7,8,9,10], function (abort) {
+        console.log(abort)
+        t.ok(sent.length < 10, 'sent is correct')
+        t.end()
+      }),
+      pull.asyncMap(function (data, cb) {
+        setImmediate(function () {
+          cb(null, data)
+        })
+      }),
+      pull.through(sent.push.bind(sent)),
+      A.drainAbort(3)
+    )
+
+  })
+}
+
 
 module.exports(id)
