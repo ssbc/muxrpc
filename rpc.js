bd52d2bbc4fd691c41a6312a34a97ec0a27b35a9
Dominic Tarr <dominic.tarr@gmail.com>
2015-10-10 06:43:50 -0700

    refactor: separate createStream

--- rpc.js
+++ rpc.js
@@ -8,26 +8,54 @@ var pull         = require('pull-stream')
 var pullWeird    = require('./pull-weird')
 var initStream   = require('./stream')
 var createApi    = require('./api')
+
 function isFunction (f) {
   return 'function' === typeof f
-}
-
-function isObject (o) {
-  return o && 'object' === typeof o
-}
-
-function isPerms (p) {
-  return (
-    p &&
-    isFunction(p.pre) &&
-    isFunction(p.test) &&
-    isFunction(p.post)
-  )
 }
 
 module.exports = function (codec) {
 
   var localApi = {}, local = {}, remoteApi = {}
+  var emitter = null
+
+  function has(type, name) {
+    return type === u.get(localApi, name)
+  }
+
+  function localCall(name, args, type) {
+
+    if(name === 'emit')
+      return emitter._emit.apply(emitter, args)
+
+    if(type === 'async')
+      if(has('sync', name)) {
+        var cb = args.pop(), value
+        try { value = u.get(local, name).apply(emitter, args) }
+        catch (err) { return cb(err) }
+        return cb(null, value)
+      }
+
+    if (!has(type, name))
+      throw new Error('no '+type+':'+name)
+    return u.get(local, name).apply(emitter, args)
+  }
+
+  function createStream (path, perms, onClose) {
+
+    perms = Permissions(perms)
+
+    function _localCall (name, args, type) {
+      var err = perms.pre(name)
+      if(err) throw err
+      return localCall(name, args, type)
+    }
+
+    var ws = initStream(_localCall, codec, onClose)
+    ws.createAccess = function () {
+      return createAccess(path, perms, ws)
+    }
+    return ws
+  }
 
   return {
     mountLocal: function (path, _api, _l) {
@@ -35,48 +63,22 @@ module.exports = function (codec) {
       u.mount(local, path, _l)
       return this
     },
+    //add a remote interface... at path.
+    //but also, should provide a function that is called
+    //when accessing down `path`, this could send a message
+    //along another stream... to do the remote call...
     mountRemote: function (path, _api) {
       u.mount(remoteApi, path, _api)
       return this
     },
+    createStream: createStream,
     access: function (path, perms) {
       return createAccess(path, perms)
     }
   }
 
-  function createAccess(path, perms) {
-
-    if     (isPerms(perms))  ;
-    else if(isObject(perms)) perms = Permissions(perms)
-    else                     perms = Permissions()
-
-    var emitter
-
-    function has(type, name) {
-      return type === u.get(localApi, name) && isFunction(u.get(local, name))
-    }
-
-    var ws, _cb
-
-    function localCall(name, args, type) {
-      var err = perms.pre(name)
-      if(err) throw err
-
-      if(name === 'emit')
-        return emitter._emit.apply(emitter, args)
-
-      if(type === 'async')
-        if(has('sync', name)) {
-          var cb = args.pop(), value
-          try { value = u.get(local, name).apply(emitter, args) }
-          catch (err) { return cb(err) }
-          return cb(null, value)
-        }
-
-      if (!has(type, name))
-        throw new Error('no '+type+':'+name)
-      return u.get(local, name).apply(emitter, args)
-    }
+  function createAccess(path, perms, _ws) {
+    var _cb
 
     function onClose (err) {
       if(emitter.closed) return
@@ -87,17 +89,14 @@ module.exports = function (codec) {
       }
     }
 
+    var ws = _ws || createStream(path, perms, onClose)
+
     //if we create the stream immediately,
     //we get the pull-stream's internal buffer
     //so all operations are queued for free!
-    ws = initStream(localCall, codec, onClose)
 
     emitter = createApi([], remoteApi, function (name, type, args, cb) {
-      var err
-      if(ws.closed) err = new Error('stream is closed')
-      else          err = perms.pre(name, args)
-      if(err) throw err
-
+      if(ws.closed) throw new Error('stream is closed')
       return ws.remoteCall(name, type, args, cb)
     })
 
@@ -108,7 +107,6 @@ module.exports = function (codec) {
       if(first) { first = false}
       else ws = ws.recreate(onClose)
 
-      emitter.close = ws.close
       return ws
     }
 
