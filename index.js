53a91df744bf44155fa3fc2cf903b870810884ed
Dominic Tarr <dominic.tarr@gmail.com>
2015-10-15 15:19:39 -0700

    create muxrpc with legacy mode, if not created old way

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var initStream   = require('./stream')
 var createApi    = require('./api')
 var createLocalCall = require('./local-api')
 
-module.exports = function (remoteApi, localApi, codec) {
+function createMuxrpc (remoteApi, localApi, local, id, perms, codec, legacy) {
   localApi = localApi || {}
   remoteApi = remoteApi || {}
   var emitter
@@ -14,33 +14,33 @@ module.exports = function (remoteApi, localApi, codec) {
   //pass the manifest to the permissions so that it can know
   //what something should be.
 
-  return function (local, perms, id) {
-    var _cb, ws
-    var context = {
-        _emit: function (event, value) {
-          emitter && emitter._emit(event, value)
-          return context
-        },
-        id: id
+  var _cb, ws
+  var context = {
+      _emit: function (event, value) {
+        emitter && emitter._emit(event, value)
+        return context
+      },
+      id: id
+    }
+
+  var ws = initStream(
+    createLocalCall(local, localApi, perms).bind(context),
+    codec, function (err) {
+      if(emitter.closed) return
+      emitter.closed = true
+      emitter.emit('closed')
+      if(_cb) {
+        var cb = _cb; _cb = null; cb(err)
       }
+    }
+  )
 
-    var ws = initStream(
-      createLocalCall(local, localApi, perms).bind(context),
-      codec, function (err) {
-        if(emitter.closed) return
-        emitter.closed = true
-        emitter.emit('closed')
-        if(_cb) {
-          var cb = _cb; _cb = null; cb(err)
-        }
-      }
-    )
+  emitter = createApi([], remoteApi, function (type, name, args, cb) {
+    if(ws.closed) throw new Error('stream is closed')
+    return ws.remoteCall(type, name, args, cb)
+  })
 
-    emitter = createApi([], remoteApi, function (type, name, args, cb) {
-      if(ws.closed) throw new Error('stream is closed')
-      return ws.remoteCall(type, name, args, cb)
-    })
-
+  if(legacy) {
     Object.__defineGetter__.call(emitter, 'id', function () {
       return context.id
     })
@@ -59,15 +59,26 @@ module.exports = function (remoteApi, localApi, codec) {
       else
         throw new Error('one stream per rpc')
     }
+  }
+  else
+    emitter.stream = ws
 
-    emitter.closed = false
+  emitter.closed = false
 
-    emitter.close = function (err, cb) {
-      ws.close(err, cb)
-      return this
-    }
+  emitter.close = function (err, cb) {
+    ws.close(err, cb)
+    return this
+  }
 
-    return emitter
+  return emitter
+
+}
+
+module.exports = function (remoteApi, localApi, codec) {
+  if(arguments > 3)
+    return createMuxrpc.apply(this, arguments)
+  return function (local, perms, id) {
+    return createMuxrpc(remoteApi, localApi, local, id, perms, codec, true)
   }
 }
 
